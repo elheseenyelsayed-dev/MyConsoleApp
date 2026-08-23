389e57254aef2976d88e3473ce175338228adec7
elheseenyelsayed-dev <elheseenyelsayed@gmail.com>
2026-08-23 19:04:59 +0300

    Add user secrets support and enhance AppSettings structure

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -3,17 +3,39 @@ using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-var appSettings = builder.Configuration
-    .GetSection("AppSettings")
-    .Get<AppSettings>();
+builder.Configuration.AddUserSecrets<Program>(
+    optional: true,
+    reloadOnChange: true
+);
+
+var appSettings = builder.Configuration  .GetSection("AppSettings") .Get<AppSettings>();
 
 Console.WriteLine($"Environment: {appSettings?.EnvironmentName}");
 Console.WriteLine($"Greeting: {appSettings?.Greeting}");
 Console.WriteLine($"Password: {appSettings?.Password}");
+Console.WriteLine($"MaxNumber: {appSettings?.MaxNumber}");
+
+var p = new Person();
+
+p.Id = 100;
+p.Age = 25;
+p.Name = "Ali";
+
+Console.WriteLine(p.Age);
+
 
 public sealed class AppSettings
 {
     public string Password { get; set; } = string.Empty;
     public string EnvironmentName { get; set; } = string.Empty;
     public string Greeting { get; set; } = string.Empty;
+    public int MaxNumber { get; set; }
+}
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
---
+++ MyConsoleApp/person.cs
@@ -0,0 +1,8 @@
+public class person
+{
+    public int Id { get; set;}
+
+    public string Name { get; set;} = string.Empty;
+
+    public int Age { get; set;}
+}
