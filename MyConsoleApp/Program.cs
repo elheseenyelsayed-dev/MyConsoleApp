fda44f595b5574c93290398a39424696b34e7fa4
Elsayoudaa <elheseeny@gmail.com>
2026-08-30 02:08:33 +0300

    Add DateOfBirth property to person class and update Program to display it

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -3,17 +3,28 @@ using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-var appSettings = builder.Configuration
-    .GetSection("AppSettings")
-    .Get<AppSettings>();
+builder.Configuration.AddUserSecrets<Program>(optional: true, reloadOnChange: true);
 
-Console.WriteLine($"Environment: {appSettings?.EnvironmentName}");
-Console.WriteLine($"Greeting: {appSettings?.Greeting}");
-Console.WriteLine($"Password: {appSettings?.Password}");
+var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>() ?? new AppSettings();
+
+Console.WriteLine($"Environment: {appSettings.EnvironmentName}");
+Console.WriteLine($"Greeting: {appSettings.Greeting}");
+Console.WriteLine($"Password: {appSettings.Password}");
+Console.WriteLine($"Max Num: {appSettings.MaxNumber}");
+
+var P = new Person();
+P.ID = 100;
+P.Age = 25;
+P.FirstName = "Ali";
+P.LastName = "Ahmed";
+P.DateOfBirth = new DateOnly(2008, 1, 1);
+
+Console.WriteLine($"DoB: {P.DateOfBirth}");
 
 public sealed class AppSettings
 {
     public string Password { get; set; } = string.Empty;
     public string EnvironmentName { get; set; } = string.Empty;
     public string Greeting { get; set; } = string.Empty;
+    public int MaxNumber {get; set; }
 }
---
+++ MyConsoleApp/person.cs
@@ -0,0 +1,9 @@
+public class Person
+{
+    public int ID {get; set;}
+    public string FirstName {get; set;} = string.Empty;
+    public int Age {get; set;}
+    public string LastName {get; set;} = string.Empty;
+    public DateOnly DateOfBirth {get; set;}
+    
+}
