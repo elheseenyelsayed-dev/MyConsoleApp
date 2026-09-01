bb1731aafb3f5dd304585081d840fe57ab0a3160
Elsayoudaa <elheseeny@gmail.com>
2026-09-01 07:38:35 +0300

    Add Nationality Property to person class

---
+++ MyConsoleApp/person.cs
@@ -0,0 +1,10 @@
+public class Person
+{
+    public int ID {get; set;}
+    public string FirstName {get; set;} = string.Empty;
+    public int Age {get; set;}
+    public string LastName {get; set;} = string.Empty;
+    public DateOnly DateOfBirth {get; set;}
+    public string Nationality {get; set;} = string.Empty;
+    
+}
